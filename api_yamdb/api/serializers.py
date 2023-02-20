23bd38ce503ede9b336744513dad51944bfb9436
Андрей Ермолин <ermolinandre1@yandex.ru>
2023-02-20 20:40:42 +0300

    Поправил код

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,36 +1,48 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 
 from reviews.models import Review, Comments
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Review."""
 
     author = serializers.SlugRelatedField(
         slug_field='username', read_only=True
     )
-    title = serializers.SlugRelatedField(
-        slug_field='name', read_only=True
-    )
+
+    def validate(self, data):
+        if self.context['request'].method != 'POST':
+            return data
+        user = self.context['request'].user
+        title_id = self.context['view'].kwargs['title_id']
+        if Review.objects.filter(author=user, title_id=title_id).exists():
+            raise serializers.ValidationError(
+                'Отзыв уже оставлен!'
+            )
+        return data
 
     class Meta:
-        fields = '__all__'
+        fields = (
+            'id',
+            'text',
+            'author',
+            'score',
+            'pub_date'
+        )
         model = Review
-        validators = [
-            UniqueTogetherValidator(
-                queryset=Review.objects.all(),
-                fields=('author', 'title'),
-                message='Двойная подписка'
-            )
-        ]
 
 
 class CommentsSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Comments."""
     author = serializers.SlugRelatedField(
-        slug_field='username',
-        read_only=True
+        read_only=True, slug_field='username'
     )
 
     class Meta:
+        fields = (
+            'id',
+            'text',
+            'author',
+            'pub_date'
+        )
         model = Comments
-        fields = '__all__'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,39 +1,61 @@
 from django.shortcuts import get_object_or_404
 from reviews.models import Review
 from rest_framework import permissions, viewsets
-from rest_framework.pagination import PageNumberPagination
 
-from .permissions import IsAuthorOrReadOnly
+from .permissions import (
+    AnonimReadOnly,
+    IsSuperUserIsAdminIsModeratorIsAuthor,
+    IsSuperUserOrIsAdminOnly
+)
 from .serializers import ReviewSerializer, CommentsSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all()
+    """Вьюсет для обьектов модели Review."""
+
     serializer_class = ReviewSerializer
-    pagination_class = PageNumberPagination
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
         IsSuperUserIsAdminIsModeratorIsAuthor
     )
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    def get_title(self):
+        """Возвращает объект текущего произведения."""
+        title_id = self.kwargs.get('title_id')
+        return get_object_or_404(Title, pk=title_id)
 
-    def perform_update(self, serializer):
-        serializer.save(author=self.request.user)
+    def get_queryset(self):
+        """Возвращает queryset c отзывами для текущего произведения."""
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        """Создает отзыв для текущего произведения,
+        где автором является текущий пользователь."""
+        serializer.save(
+            author=self.request.user,
+            title=self.get_title()
+        )
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
+    """Вьюсет для обьектов модели Comment."""
+
     serializer_class = CommentsSerializer
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
-        IsAuthorOrReadOnly
+        IsSuperUserIsAdminIsModeratorIsAuthor
     )
 
     def get_queryset(self):
-        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        review = get_object_or_404(
+            Review, id=self.kwargs['review_id'],
+            title__id=self.kwargs['title_id']
+        )
         return review.comments.all()
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, id=self.kwargs.get('review_id'))
+        review = get_object_or_404(
+            Review, id=self.kwargs['review_id'],
+            title__id=self.kwargs['title_id']
+        )
         serializer.save(author=self.request.user, review=review)
